f1411600d789465feacc6639eafb3a3894cbe35c
Alvaro Miranda <kikitux@gmail.com>
2014-12-07 21:55:30 +1300

    add clean.sh

---
+++ stagefiles/clean.sh
@@ -0,0 +1,12 @@
+if [ "$1" == "YES" ]; then
+  find /etc -name '*ohasd*' -exec rm {} \;
+  find /etc -name '*tfa**' -exec rm {} \;
+  rm -fr /u01/app /etc/oracle 2>/dev/null
+  for x in /dev/sd{c..z}; do
+    blkid $x* 2>/dev/null && dd if=/dev/zero of=$x bs=1M count=10
+  done
+  echo "halting $HOSTNAME"
+  halt
+else
+  echo "This script require YES as argument"
+fi
