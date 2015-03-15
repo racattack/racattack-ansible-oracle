d0119ed1ca7bdbd97a66f335158112cc18cb4c6e
Alvaro Miranda <kikitux@gmail.com>
2015-03-15 13:37:05 +1300

    upate clean.sh for .oracle
    rm -fr /tmp/.oracle/ /var/tmp/.oracle/

--- stagefiles/clean.sh
+++ stagefiles/clean.sh
@@ -1,7 +1,7 @@
 if [ "$1" == "YES" ]; then
   find /etc -name '*ohasd*' -exec rm {} \;
   find /etc -name '*tfa**' -exec rm {} \;
-  rm -fr /u01/app /u01/stage /etc/oracle /etc/oratab /etc/oraInst.loc 2>/dev/null
+  rm -fr /u01/app /u01/stage /etc/oracle /etc/oratab /etc/oraInst.loc /tmp/.oracle/ /var/tmp/.oracle 2>/dev/null
   for x in /dev/sd{c..z}; do
     blkid $x* 2>/dev/null && dd if=/dev/zero of=$x bs=1M count=10
   done
